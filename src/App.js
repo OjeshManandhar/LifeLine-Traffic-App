7f258430bda2b9719f661601c00bb1fa9656a9f4
OjeshManandhar <ozes.manandhar@gmail.com>
2020-10-15 09:05:02 +0545

    Hide Splash screen from Login

--- src/App.js
+++ src/App.js
@@ -8,11 +8,8 @@
 import 'react-native-gesture-handler';
 // Note: If you skip this step, your app may crash in production even if it works fine in development.
 
-import React, { useEffect } from 'react';
+import React from 'react';
 import { StatusBar } from 'react-native';
-
-// packages
-import SplashScreen from 'react-native-splash-screen';
 
 // navigator
 import Navigator from 'navigator';
@@ -23,9 +20,6 @@ import UserLocation from 'utils/userLocation';
 // UserLocation.init();
 
 function App() {
-  // Hide Splash screen
-  useEffect(SplashScreen.hide, []);
-
   return (
     <React.Fragment>
       <StatusBar barStyle='dark-content' />
--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Text, View, Button } from 'react-native';
+
+// packages
+import SplashScreen from 'react-native-splash-screen';
 
 // global
 import Routes from 'global/routes';
@@ -8,6 +11,9 @@ import Routes from 'global/routes';
 import { USER_TOKEN_KEY } from '@env';
 
 function Login({ navigation }) {
+  // Hide Splash screen
+  useEffect(SplashScreen.hide, []);
+
   return (
     <View>
       <Text>Login</Text>
