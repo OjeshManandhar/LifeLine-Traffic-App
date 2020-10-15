ddceb22d621508335dab6f42ac15e97f37c466e6
OjeshManandhar <ozes.manandhar@gmail.com>
2020-10-15 09:56:48 +0545

    Hide Splash screen after checking token

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@
 import 'react-native-gesture-handler';
 // Note: If you skip this step, your app may crash in production even if it works fine in development.
 
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { StatusBar } from 'react-native';
 
 // packages
@@ -17,14 +17,45 @@ import SplashScreen from 'react-native-splash-screen';
 // navigator
 import Navigator from 'navigator';
 
+// dummy_api
+import { checkToken } from 'dummy_api';
+
 // utils
+import UserToken from 'utils/userToken';
 import UserLocation from 'utils/userLocation';
 
 // UserLocation.init();
 
 function App() {
-  // Hide Splash screen
-  useEffect(SplashScreen.hide, []);
+  const [isReady, setIsReady] = useState(false);
+
+  async function loadResources() {
+    await UserToken.init();
+
+    const userToken = await UserToken.get();
+
+    console.log('userToken:', userToken);
+
+    if (userToken) {
+      const { valid } = await checkToken(userToken);
+      if (!valid) {
+        await UserToken.delete();
+      }
+    }
+  }
+
+  // Prevent Auto hide of Splash Screen
+  useEffect(() => {
+    (async function () {
+      await loadResources();
+
+      setIsReady(true);
+
+      SplashScreen.hide();
+    })();
+  }, [setIsReady]);
+
+  // if (!isReady) return <></>;
 
   return (
     <React.Fragment>
