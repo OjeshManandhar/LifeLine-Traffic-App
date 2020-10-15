d1ff46a9d900e5c9d555d1095f4245524f071b9a
OjeshManandhar <ozes.manandhar@gmail.com>
2020-10-15 08:39:40 +0545

    Working .env

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -4,10 +4,15 @@ import { Text, View, Button } from 'react-native';
 // global
 import Routes from 'global/routes';
 
+// env
+import { USER_TOKEN_KEY } from '@env';
+
 function Login({ navigation }) {
   return (
     <View>
       <Text>Login</Text>
+
+      <Text>User token: {USER_TOKEN_KEY}</Text>
 
       <Button
         title='MapScreen'
