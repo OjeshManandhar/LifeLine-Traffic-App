29b49a681ab02662911fdd756ccef781825adce1
OjeshManandhar <ozes.manandhar@gmail.com>
2020-10-15 11:47:16 +0545

    Add global/strings

---
+++ src/global/strings.js
@@ -0,0 +1,33 @@
+export const LoginText = {
+  button: 'Log In',
+  form: {
+    phoneNumber: 'Phone Number',
+    password: 'Password'
+  },
+  signUp: {
+    title: 'Sign Up',
+    detail: 'Please contact the admin to register you to the App. \nThank You!'
+  },
+  errorText: {
+    noAccount: 'Phone number is not registered yet',
+    noNetwork: 'Please check your internet connection',
+    phonePassError: 'Phone number and Password does not match'
+  }
+};
+
+export const MapScreenText = {
+  logoutAlert: {
+    title: 'Log Out',
+    description: 'Are you sure you want to Log out?',
+    negative: 'Cancel',
+    positive: 'Log out'
+  }
+};
+
+export const AccountViewText = {
+  button: 'Log Out',
+  accountType: {
+    driver: 'Driver Account',
+    traffic: 'Traffic Account'
+  }
+};
