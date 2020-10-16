78ef868e8d58400ff284e200406e7a4b71f8dee8
OjeshManandhar <ozes.manandhar@gmail.com>
2020-10-16 10:19:04 +0545

    Add avatar button

--- src/components/MapView/index.js
+++ src/components/MapView/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Image, Button, Pressable } from 'react-native';
+
+// components
+import Map from 'components/Map';
+import Text from 'components/Text';
+
+// assets
+import account from 'assets/images/dead.png';
+
+// styles
+import styles from './styles';
 
 function MapView({ toAccount, setBackHandler }) {
   return (
-    <View>
-      <Text>MapView</Text>
+    <View style={styles.container}>
+      <Map />
 
-      <Button title='Account' onPress={() => toAccount(123)} />
+      <Pressable style={styles.avatarContainer} onPress={() => toAccount(123)}>
+        <Image source={account} style={styles.avatar} />
+      </Pressable>
     </View>
   );
 }
