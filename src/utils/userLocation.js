f9735a0b0899666fd0ecfcd68b4bf084e78cfdfb
OjeshManandhar <ozes.manandhar@gmail.com>
2021-02-07 12:36:55 +0545

    Update UserLocation

--- src/utils/userLocation.js
+++ src/utils/userLocation.js
@@ -3,9 +3,29 @@ import Geolocation from '@react-native-community/geolocation';
 
 // utils
 import socket from 'utils/socket';
+import UserInfo from 'utils/userInfo';
 
 // global
 import { SocketText } from 'global/strings';
+
+function emitLocation(location, operation) {
+  const info = UserInfo.getInfo();
+
+  socket.emit(SocketText.events.driverLocation, {
+    operation,
+    driver_gps: {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: location
+      },
+      properties: {
+        role: info.role,
+        contact: info.contact
+      }
+    }
+  });
+}
 
 class UserLocation {
   #userLocation = null;
@@ -18,13 +38,7 @@ class UserLocation {
       sucess => {
         this.#userLocation = [sucess.coords.longitude, sucess.coords.latitude];
 
-        socket.emit(SocketText.events.trafficLocation, {
-          gps: {
-            userId: 'DeadSkull',
-            location: this.#userLocation
-          },
-          operation: SocketText.operations.create
-        });
+        emitLocation(this.#userLocation, SocketText.operations.create);
       },
       error => {
         console.log('getCurrentPosition error:', error);
@@ -36,24 +50,18 @@ class UserLocation {
 
     this.#watchId = Geolocation.watchPosition(
       sucess => {
-        console.log('Geolocation.watchPosition() sucess:', sucess);
+        // console.log('Geolocation.watchPosition() sucess:', sucess);
         this.#userLocation = [sucess.coords.longitude, sucess.coords.latitude];
 
-        socket.emit(SocketText.events.trafficLocation, {
-          gps: {
-            userId: 'DeadSkull',
-            location: this.#userLocation
-          },
-          operation: SocketText.operations.update
-        });
+        emitLocation(this.#userLocation, SocketText.operations.update);
       },
       error => {
         console.log('watchPosition error:', error);
       },
       {
-        distanceFilter: 10, // 10 meters
-        timeout: 10 * 60 * 1000, // 10 mins
-        maximumAge: 20 * 60 * 1000, // 20 mins
+        distanceFilter: 5, // 5 meters
+        timeout: 2.5 * 60 * 1000, // 2.5 mins
+        maximumAge: 5 * 60 * 1000, // 5 mins
         enableHighAccuracy: true
       }
     );
@@ -68,13 +76,7 @@ class UserLocation {
     Geolocation.clearWatch(this.#watchId);
     Geolocation.stopObserving();
 
-    socket.emit(SocketText.events.trafficLocation, {
-      gps: {
-        userId: 'DeadSkull',
-        location: this.#userLocation
-      },
-      operation: SocketText.operations.delete
-    });
+    emitLocation(this.#userLocation, SocketText.operations.delete);
   }
 }
 
